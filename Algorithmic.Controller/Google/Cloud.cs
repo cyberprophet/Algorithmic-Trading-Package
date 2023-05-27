d1f369ce1554a1aaba15a0cf226f169aa610ba82
SANGWOO PARK <cyberprophet@hanmail.net>
2023-05-28 00:07:09 +0900

    make the library that controls sound

---
+++ Algorithmic.Controller/Google/Cloud.cs
@@ -0,0 +1,28 @@
+﻿using Google.Cloud.Speech.V1;
+
+using System.Reflection;
+
+namespace ShareInvest.Google;
+
+public static class Cloud
+{
+    public static IEnumerable<string> GetLanguageCodes()
+    {
+        foreach (var property in typeof(LanguageCodes).GetProperties(BindingFlags.Static | BindingFlags.Public))
+        {
+            if (property.PropertyType.IsClass)
+            {
+                foreach (var p in property.GetType()
+                                          .GetProperties(BindingFlags.Static | BindingFlags.Public))
+                {
+                    yield return p.Name;
+                }
+                continue;
+            }
+            if (property.PropertyType == typeof(string))
+            {
+                yield return property.Name;
+            }
+        }
+    }
+}
---
+++ Algorithmic.Controller/Google/TextToSpeech.cs
@@ -0,0 +1,58 @@
+﻿using Google.Cloud.Speech.V1;
+using Google.Cloud.TextToSpeech.V1;
+
+using System.Text;
+
+namespace ShareInvest.Google;
+
+public class TextToSpeech
+{
+    public async Task<Stream> SynthesizeSpeechAsync(Stream stream, string text)
+    {
+        foreach (var voice in (await tts.ListVoicesAsync(VoiceSelection.LanguageCode)).Voices)
+        {
+            if (voice.SsmlGender == VoiceSelection.SsmlGender)
+            {
+                VoiceSelection.Name = voice.Name;
+
+                if (stream.Length % 3 == DateTime.Now.Ticks % 3)
+                {
+                    break;
+                }
+            }
+        }
+        var input = new SynthesisInput
+        {
+            Text = text,
+        };
+        var res = await tts.SynthesizeSpeechAsync(input, VoiceSelection, config);
+
+        res.AudioContent.WriteTo(stream);
+
+        stream.Position = 0;
+
+        return stream;
+    }
+    public VoiceSelectionParams VoiceSelection
+    {
+        get;
+    }
+    public TextToSpeech(byte[] json)
+    {
+        VoiceSelection = new VoiceSelectionParams
+        {
+            LanguageCode = LanguageCodes.English.UnitedStates
+        };
+        config = new AudioConfig
+        {
+            AudioEncoding = AudioEncoding.Linear16
+        };
+        tts = new TextToSpeechClientBuilder
+        {
+            JsonCredentials = Encoding.UTF8.GetString(json)
+        }
+        .Build();
+    }
+    readonly AudioConfig config;
+    readonly TextToSpeechClient tts;
+}
