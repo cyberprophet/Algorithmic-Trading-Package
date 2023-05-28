a95292d567f83438fdbcba85802077ec7089ebcc
SANGWOO PARK <cyberprophet@hanmail.net>
2023-05-28 12:54:06 +0900

    error in the update process

---
+++ Algorithmic.Controller/Microsoft/Models/Chart.cs
@@ -0,0 +1,29 @@
+﻿namespace ShareInvest.Microsoft.Models;
+
+public class Chart
+{
+    public string? Date
+    {
+        get; set;
+    }
+    public int Close
+    {
+        get; set;
+    }
+    public int High
+    {
+        get; set;
+    }
+    public int Low
+    {
+        get; set;
+    }
+    public int Start
+    {
+        get; set;
+    }
+    public long Volume
+    {
+        get; set;
+    }
+}
