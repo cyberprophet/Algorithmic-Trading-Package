92bccd4029aa37a2f0943a0842d48f4745b1a721
SANGWOO PARK <cyberprophet@hanmail.net>
2023-05-31 00:24:41 +0900

    into a labyrinth of data extraction

--- Algorithmic.Controller/Microsoft/MachineLearning.cs
+++ Algorithmic.Controller/Microsoft/MachineLearning.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.ML;
 
+using ShareInvest.Microsoft.Models;
+
 namespace ShareInvest.Microsoft;
 
 public abstract class MachineLearning : IDisposable
@@ -8,11 +10,41 @@ public abstract class MachineLearning : IDisposable
 
     public virtual void Dispose() => GC.SuppressFinalize(this);
 
-    public virtual void Learning<T>(IEnumerable<T> enumerable) where T : class
+    public virtual void Learning(IEnumerable<Chart> enumerable)
     {
         IDataView dataView = context.Data.LoadFromEnumerable(enumerable);
+        
+        var columnPair = new[]
+        {
+            new InputOutputColumnPair(nameof(Chart.Start)),
+            new InputOutputColumnPair(nameof(Chart.High)),
+            new InputOutputColumnPair(nameof(Chart.Low)),
+            new InputOutputColumnPair(nameof(Chart.Close)),
+            new InputOutputColumnPair(nameof(Chart.Volume))
+        };
+        var normalize = context.Transforms.NormalizeMinMax(columnPair, fixZero: false);
 
+        var normalizeTransform = normalize.Fit(dataView);
 
+        var dataSplit = context.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+        var dataPrepEstimator = context.Transforms.Concatenate("Features", nameof(Chart.Start), nameof(Chart.High), nameof(Chart.Low), nameof(Chart.Close), nameof(Chart.Volume));
+
+        var dataPrepTransformer = dataPrepEstimator.Fit(dataSplit.TrainSet);
+
+        var transformedTrainingData = dataPrepTransformer.Transform(dataSplit.TrainSet);
+
+        var estimator = context.Regression.Trainers.Sdca();
+
+        var trainedModel = estimator.Fit(transformedTrainingData);
+
+        var transformedTestData = dataPrepTransformer.Transform(dataSplit.TestSet);
+
+        var testDataPredictions = trainedModel.Transform(transformedTestData);
+
+        var metrics = context.Regression.Evaluate(testDataPredictions);
+
+        System.Diagnostics.Debug.WriteLine(metrics);
     }
     public MachineLearning(int? seed = null)
     {
