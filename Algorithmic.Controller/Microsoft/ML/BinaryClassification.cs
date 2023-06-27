a826d71d44ab19b7f69cf464f12a04da68fc0dff
SANGWOO PARK <cyberprophet@hanmail.net>
2023-06-27 22:56:07 +0900

    lack of understanding of data preprocessing

---
+++ Algorithmic.Controller/Microsoft/ML/BinaryClassification.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML;
+
+using ShareInvest.Microsoft.Models;
+
+namespace ShareInvest.Microsoft.ML;
+
+public class BinaryClassification : ModelBuilder
+{
+    public override dynamic Evaluate<T>(T data)
+    {
+        throw new NotImplementedException();
+    }
+    public override void Learning<T>(IEnumerable<T> enumerable)
+    {
+        IDataView dataView = context.Data.LoadFromEnumerable(enumerable);
+
+        IEstimator<ITransformer> pipeline =
+
+            context.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: nameof(InputStockData.Suitable));
+
+        pipeline.Fit(dataView);
+    }
+    public BinaryClassification() : base()
+    {
+
+    }
+}
