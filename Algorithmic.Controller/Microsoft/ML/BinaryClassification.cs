6d64a860c85f92578b63a5de760f5faf952e8cc4
SANGWOO PARK <cyberprophet@hanmail.net>
2023-07-19 01:05:48 +0900

    do pre-deployment trials

--- Algorithmic.Controller/Microsoft/ML/BinaryClassification.cs
+++ Algorithmic.Controller/Microsoft/ML/BinaryClassification.cs
@@ -1,27 +1,59 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
 
-using ShareInvest.Microsoft.Models;
+using Newtonsoft.Json;
+
+using ShareInvest.ML.Models;
 
 namespace ShareInvest.Microsoft.ML;
 
 public class BinaryClassification : ModelBuilder
 {
-    public override dynamic Evaluate<T>(T data)
+    public override void Evaluate(IDataView predictions)
     {
-        throw new NotImplementedException();
+        var metrics = context.BinaryClassification.Evaluate(predictions, labelColumnName: labelColumnName);
+
+        Console.WriteLine(JsonConvert.SerializeObject(metrics, Formatting.Indented));
     }
-    public override void Learning<T>(IEnumerable<T> enumerable)
+    public override IDataView Learning<T>(IEnumerable<T> enumerable)
     {
-        IDataView dataView = context.Data.LoadFromEnumerable(enumerable);
+        IDataView dataView = context.Data.LoadFromEnumerable(enumerable as IEnumerable<InputConditionData>);
 
-        IEstimator<ITransformer> pipeline =
+        var estimator = context.Transforms.Conversion.ConvertType(nameof(InputConditionData.ClosePrices), outputKind: DataKind.Single)
 
-            context.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: nameof(InputStockData.Suitable));
+            .Append(context.Transforms.Conversion.ConvertType(nameof(InputConditionData.HighPrices), outputKind: DataKind.Single))
+            .Append(context.Transforms.Conversion.ConvertType(nameof(InputConditionData.LowPrices), outputKind: DataKind.Single))
+            .Append(context.Transforms.Conversion.ConvertType(nameof(InputConditionData.OpenPrices), outputKind: DataKind.Single))
+            .Append(context.Transforms.Conversion.ConvertType(nameof(InputConditionData.Volumes), outputKind: DataKind.Single))
+            .Append(context.Transforms.Conversion.ConvertType(nameof(InputConditionData.DateTimes), outputKind: DataKind.Single))
+            .Append(context.Transforms.Concatenate(featureColumnName, nameof(InputConditionData.ClosePrices),
+                                                                      nameof(InputConditionData.HighPrices),
+                                                                      nameof(InputConditionData.LowPrices),
+                                                                      nameof(InputConditionData.OpenPrices),
+                                                                      nameof(InputConditionData.Volumes),
+                                                                      nameof(InputConditionData.DateTimes)));
 
-        pipeline.Fit(dataView);
+        var splitDataView = context.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+        var pipeline = estimator.Append(context.BinaryClassification.Trainers.SdcaLogisticRegression(options));
+
+        return pipeline.Fit(splitDataView.TrainSet).Transform(splitDataView.TestSet);
     }
-    public BinaryClassification() : base()
+    public BinaryClassification(int? seed = null, int? gpuDeviceId = null) : base(seed, gpuDeviceId)
     {
-
+        options = new SdcaLogisticRegressionBinaryTrainer.Options
+        {
+            MaximumNumberOfIterations =
+#if DEBUG
+                                        default
+#else
+                                        0x800
+#endif
+                                        ,
+            LabelColumnName = labelColumnName,
+            FeatureColumnName = featureColumnName
+        };
     }
+    readonly SdcaLogisticRegressionBinaryTrainer.Options options;
 }
--- Algorithmic.Controller/Microsoft/ModelBuilder.cs
+++ Algorithmic.Controller/Microsoft/ModelBuilder.cs
@@ -4,15 +4,19 @@ namespace ShareInvest.Microsoft;
 
 public abstract class ModelBuilder : IDisposable
 {
-    public abstract dynamic? Evaluate<T>(T data) where T : class;
+    public abstract void Evaluate(IDataView predictions);
 
-    public abstract void Learning<T>(IEnumerable<T> enumerable) where T : class;
+    public abstract IDataView Learning<T>(IEnumerable<T> enumerable) where T : class;
 
     public virtual void Dispose() => GC.SuppressFinalize(this);
 
-    public ModelBuilder(int? seed = null)
+    public ModelBuilder(int? seed, int? gpuDeviceId)
     {
         context = seed != null ? new MLContext(seed) : new MLContext();
+
+        context.GpuDeviceId = gpuDeviceId;
     }
     protected internal readonly MLContext context;
+    protected internal const string labelColumnName = "Label";
+    protected internal const string featureColumnName = "Features";
 }
